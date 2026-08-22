20ce644644c536b41df3dff543f94d282cb10994
luckylk007 <luckykumar21099@gmail.com>
2026-08-22 17:51:34 +0530

    chore: disabled browser/proxy caching via Cache-Control headers and purged unused asset files

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,27 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, max-age=0, must-revalidate",
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+          {
+            key: "Expires",
+            value: "0",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
